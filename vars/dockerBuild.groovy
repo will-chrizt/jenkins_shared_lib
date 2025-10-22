8ee990ebdb4e666ffdae52ca1823ff0cf8f41f5c
Willchrist <willchristsebastian@gmail.com>
2025-10-22 13:49:01 +0530

    removed sudo

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,6 @@
  def call(String project, String ImageTag, String hubUser){
     
      sh """
-      sudo su
       docker image build -t ${hubUser}/${project} . 
       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
