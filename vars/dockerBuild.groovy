914ea4981a9218ec6c7a69407e2dc3e4f0058789
Willchrist <willchristsebastian@gmail.com>
2025-10-22 13:41:22 +0530

    added sudo su

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,7 @@
  def call(String project, String ImageTag, String hubUser){
     
      sh """
+      sudo su
       docker image build -t ${hubUser}/${project} . 
       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
