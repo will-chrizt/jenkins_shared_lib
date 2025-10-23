a2a2ade19b85d351048c1a87ff008ee2098600f7
Willchrist <willchristsebastian@gmail.com>
2025-10-23 13:48:01 +0530

    clenaup edit

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,6 +1,6 @@
  def call(String project, String ImageTag, String hubUser){
      sh """
       docker rmi ${hubUser}/${project}:${ImageTag}
-      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+  
      """
  }
