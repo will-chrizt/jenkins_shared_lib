cceb6f099fc6658497e7c962860efc630f1d2100
Willchrist <willchristsebastian@gmail.com>
2025-10-23 15:11:10 +0530

    docer permiison scrip removed

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,6 @@
  def call(String project, String ImageTag, String hubUser){
     
      sh """
-      chmod 777 /var/run/docker.sock
       docker image build -t ${hubUser}/${project} . 
       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
